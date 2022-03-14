4d4c2848f1aa32e651dd31e519dcfcfc5ef072f6
AgnethaDMello <agnethad@nycstudents.net>
2022-03-14 13:58:58 -0400

    e

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,9 +1,33 @@
 <template>
-  <Tutorial/>
+  <Tutorial />
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
-}
+  name: "IndexPage",
+};
 </script>
+
+<style>
+body,
+html,
+* {
+  margin: 0;
+  padding: 0;
+  font-size: 62.5%;
+  box-sizing: border-box;
+}
+:root {
+  --bg-blue: ;
+  --header-blue: ;
+  --light-blue: ;
+  --bg-wave: ;
+  --black: ;
+
+  --h1: 9rem;
+  --h2: 2.3rem;
+  --h3: 1.5rem;
+
+  --primary: var(--barely-blue);
+}
+</style>
