c5338b8214294c9c21ce8ba76045d146cc4412f0
AgnethaDMello <agnethad@nycstudents.net>
2022-04-13 13:57:59 -0400

    poisoned m&ms

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -4,28 +4,36 @@
       <h1>Beach Please</h1>
       <div id="titles">
         <div class="shells">
-          <img class="seashell1" src="../images/seashell.PNG" />
-          <img class="seashell2" src="../images/seashell.PNG" />
+          <img class="seashell1" src="../images/misc./seashell.PNG" />
+          <img class="seashell2" src="../images/misc./seashell.PNG" />
         </div>
         <div class="cham">
           <h2 class="names">Champagne Beach</h2>
-          <img class="bigpics" src="../images/champagne beach.jpg" />
+          <img class="bigpics" src="../images/Beaches/champagne beach.jpg" />
+          <img class="bigpics" src="../images/Food/Champagne beach food.jpg" />
         </div>
         <div class="ora">
           <h2 class="names">Ora Beach</h2>
-          <img class="bigpics" src="../images/ora beach.jpg" />
+          <img class="bigpics" src="../images/Beaches/ora beach.jpg" />
+          <img class="bigpics" src="../images/Food/Ora beach food.jpg" />
         </div>
         <div class="stars">
           <h2 class="names">Sea of Stars</h2>
-          <img class="bigpics" src="../images/maldives.jpg" />
+          <img class="bigpics" src="../images/Beaches/maldives.jpg" />
+          <img class="bigpics" src="../images/Food/Sea of Stars food.jpg" />
         </div>
         <div class="copa">
           <h2 class="names">Copacabana</h2>
-          <img class="bigpics" src="../images/copacabana.jpg" />
+          <img class="bigpics" src="../images/Beaches/copacabana.jpg" />
+          <img class="bigpics" src="../images/Food/Copacabana food.jpg" />
         </div>
         <div class="rena">
           <h2 class="names">Renaissance Island</h2>
-          <img class="bigpics" src="../images/ri.png" />
+          <img class="bigpics" src="../images/Beaches/ri.png" />
+          <img
+            class="bigpics"
+            src="../images/Food/Renaissance Island food.jpg"
+          />
         </div>
       </div>
     </div>
