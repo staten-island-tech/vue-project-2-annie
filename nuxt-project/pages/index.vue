0f2bd25659e78fbcaf8b9232f064f211238d9300
AnisaLubonja <90656926+AnisaLubonja@users.noreply.github.com>
2022-04-12 23:55:08 -0400

    activislays

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -10,22 +10,37 @@
         <div class="cham">
           <h2 class="names">Champagne Beach</h2>
           <img class="bigpics" src="../images/champagne beach.jpg" />
+          <img class="" src="../images/Activities/Beach Zumba.jpg" />
+          <img class="" src="../images/Activities/Sand art.jpg" />
+          <img class="" src="../images/Activities/Frisbee.jpg" />
         </div>
         <div class="ora">
           <h2 class="names">Ora Beach</h2>
           <img class="bigpics" src="../images/ora beach.jpg" />
+          <img class="" src="../images/Activities/Castle construction.jpg" />
+          <img class="" src="../images/Activities/Jewelry Making.jpg" />
+          <img class="" src="../images/Activities/Water polo.jpg" />
         </div>
         <div class="stars">
           <h2 class="names">Sea of Stars</h2>
           <img class="bigpics" src="../images/maldives.jpg" />
+          <img class="" src="../images/Activities/Beach Bonfires.jpg" />
+          <img class="" src="../images/Activities/Sea star gazing.jpg" />
+          <img class="" src="../images/Activities/Fishing.jpg" />
         </div>
         <div class="copa">
           <h2 class="names">Copacabana</h2>
           <img class="bigpics" src="../images/copacabana.jpg" />
+          <img class="" src="../images/Activities/Beach volleyball.jpg" />
+          <img class="" src="../images/Activities/Snorkling.jpg" />
+          <img class="" src="../images/Activities/Surfing.jpg" />
         </div>
         <div class="rena">
           <h2 class="names">Renaissance Island</h2>
           <img class="bigpics" src="../images/ri.png" />
+          <img class="" src="../images/Activities/Flamingo watching.jpg" />
+          <img class="" src="../images/Activities/Jeep safari.jpg" />
+          <img class="" src="../images/Activities/Beach reading.jpg" />
         </div>
       </div>
     </div>
