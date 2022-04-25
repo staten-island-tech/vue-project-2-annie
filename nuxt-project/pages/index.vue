084d608f2208d8f4909ea9d0973ca5e9a788f8bb
AgnethaDMello <agnethad@nycstudents.net>
2022-04-25 13:45:10 -0400

    omg maggies bday woo

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -10,30 +10,70 @@
         <div class="cham">
           <h2 class="names">Champagne Beach</h2>
           <img class="bigpics" src="../images/Beaches/champagne beach.jpg" />
-          <img class="bigpics" src="../images/Food/Champagne beach food.jpg" />
+          <img class="foodpics" src="../images/Food/Champagne beach food.jpg" />
+          <div>
+            <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
+            <img class="actpics" src="../images/Activities/Sand art.jpg" />
+            <img class="actpics" src="../images/Activities/Frisbee.jpg" />
+          </div>
         </div>
         <div class="ora">
           <h2 class="names">Ora Beach</h2>
           <img class="bigpics" src="../images/Beaches/ora beach.jpg" />
-          <img class="bigpics" src="../images/Food/Ora beach food.jpg" />
+          <img class="foodpics" src="../images/Food/Ora beach food.jpg" />
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Castle construction.jpg"
+            />
+            <img
+              class="actpics"
+              src="../images/Activities/Jewelry Making.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Water Polo.jpg" />
+          </div>
         </div>
         <div class="stars">
           <h2 class="names">Sea of Stars</h2>
           <img class="bigpics" src="../images/Beaches/maldives.jpg" />
-          <img class="bigpics" src="../images/Food/Sea of Stars food.jpg" />
+          <img class="foodpics" src="../images/Food/Sea of Stars food.jpg" />
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Beach Bonfires.jpg"
+            />
+            <img
+              class="actpics"
+              src="../images/Activities/Sea star gazing.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Fishing.jpg" />
+          </div>
         </div>
         <div class="copa">
           <h2 class="names">Copacabana</h2>
           <img class="bigpics" src="../images/Beaches/copacabana.jpg" />
-          <img class="bigpics" src="../images/Food/Copacabana food.jpg" />
+          <img class="foodpics" src="../images/Food/Copacabana food.jpg" />
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Beach volleyball.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Snorkling.jpg" />
+            <img class="actpics" src="../images/Activities/Surfing.jpg" />
+          </div>
         </div>
         <div class="rena">
           <h2 class="names">Renaissance Island</h2>
           <img class="bigpics" src="../images/Beaches/ri.png" />
           <img
-            class="bigpics"
+            class="foodpics"
             src="../images/Food/Renaissance Island food.jpg"
           />
+          <div>
+            <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
+            <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
+            <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
+          </div>
         </div>
       </div>
     </div>
