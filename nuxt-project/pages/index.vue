728f2777ad139e5b0d478389ce925ca0740b029e
AgnethaDMello <agnethad@nycstudents.net>
2022-05-12 13:53:40 -0400

    WHY IS IT SHOWING FOR A SPLIT SECOND BEFORE DISAPPEARING THEN REAPPEARING.

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -2,88 +2,99 @@
   <body id="body">
     <div>
       <h1 id="maintitle">Beach Please</h1>
-      <div id="titles">
-        <div class="shells">
-          <img class="seashell1" src="../images/misc./seashell.PNG" />
-          <img class="seashell2" src="../images/misc./seashell.PNG" />
+      <div id="gallery">
+        <div id="titles">
+          <div class="shells">
+            <img class="seashell1" src="../images/misc./seashell.PNG" />
+            <img class="seashell2" src="../images/misc./seashell.PNG" />
+          </div>
+          <div class="cham">
+            <h2 class="names" id="cb">Champagne Beach</h2>
+            <img class="bigpics" src="../images/Beaches/champagne beach.jpg" />
+            <img class="sand" src="../images/Beaches/sand.jpg" />
+          </div>
+          <h2 class="activities">Activities</h2>
+          <div>
+            <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
+            <img class="actpics" src="../images/Activities/Sand art.jpg" />
+            <img class="actpics" src="../images/Activities/Frisbee.jpg" />
+          </div>
+          <h2 class="reviews">Reviews</h2>
+          <img class="foodpics" src="../images/Food/Champagne beach food.jpg" />
         </div>
-        <div class="cham">
-          <h2 class="names" id="cb">Champagne Beach</h2>
-          <img class="bigpics" src="../images/Beaches/champagne beach.jpg" />
+        <div class="ora" id="ob">
+          <h2 class="names">Ora Beach</h2>
+          <img class="bigpics" src="../images/Beaches/ora beach.jpg" />
           <img class="sand" src="../images/Beaches/sand.jpg" />
+          <h2 class="activities">Activities</h2>
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Castle construction.jpg"
+            />
+            <img
+              class="actpics"
+              src="../images/Activities/Jewelry Making.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Water Polo.jpg" />
+          </div>
+          <h2 class="reviews">Reviews</h2>
+          <img class="foodpics" src="../images/Food/Ora beach food.jpg" />
         </div>
-        <h2 class="activities">Activities</h2>
-        <div>
-          <img class="actpics" src="../images/Activities/Beach zumba.jpg" />
-          <img class="actpics" src="../images/Activities/Sand art.jpg" />
-          <img class="actpics" src="../images/Activities/Frisbee.jpg" />
+        <div class="stars">
+          <h2 class="names" id="sos">Sea of Stars</h2>
+          <img class="bigpics" src="../images/Beaches/maldives.jpg" />
+          <img class="sand" src="../images/Beaches/sand.jpg" />
+          <h2 class="activities">Activities</h2>
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Beach Bonfires.jpg"
+            />
+            <img
+              class="actpics"
+              src="../images/Activities/Sea star gazing.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Fishing.jpg" />
+          </div>
+          <h2 class="reviews">Reviews</h2>
+          <img class="foodpics" src="../images/Food/Sea of Stars food.jpg" />
         </div>
-        <h2 class="reviews">Reviews</h2>
-        <img class="foodpics" src="../images/Food/Champagne beach food.jpg" />
-      </div>
-      <div class="ora" id="ob">
-        <h2 class="names">Ora Beach</h2>
-        <img class="bigpics" src="../images/Beaches/ora beach.jpg" />
-        <img class="sand" src="../images/Beaches/sand.jpg" />
-        <h2 class="activities">Activities</h2>
-        <div>
+        <div class="copa">
+          <h2 class="names" id="c">Copacabana</h2>
+          <img class="bigpics" src="../images/Beaches/copacabana.jpg" />
+          <img class="sand" src="../images/Beaches/sand.jpg" />
+          <h2 class="activities">Activities</h2>
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Beach volleyball.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Snorkling.jpg" />
+            <img class="actpics" src="../images/Activities/Surfing.jpg" />
+          </div>
+          <h2 class="reviews">Reviews</h2>
+          <img class="foodpics" src="../images/Food/Copacabana food.jpg" />
+        </div>
+        <div class="rena">
+          <h2 class="names" id="ri">Renaissance Island</h2>
+          <img class="bigpics" src="../images/Beaches/ri.png" />
+          <img class="sand" src="../images/Beaches/sand.jpg" />
+          <h2 class="activities">Activities</h2>
+          <div>
+            <img
+              class="actpics"
+              src="../images/Activities/Flamingo Watching.jpg"
+            />
+            <img class="actpics" src="../images/Activities/Jeep Safari.jpg" />
+            <img class="actpics" src="../images/Activities/Beach Reading.jpg" />
+          </div>
+          <h2 class="reviews">Reviews</h2>
           <img
-            class="actpics"
-            src="../images/Activities/Castle construction.jpg"
+            class="foodpics"
+            src="../images/Food/Renaissance Island food.jpg"
           />
-          <img class="actpics" src="../images/Activities/Jewelry Making.jpg" />
-          <img class="actpics" src="../images/Activities/Water Polo.jpg" />
         </div>
-        <h2 class="reviews">Reviews</h2>
-        <img class="foodpics" src="../images/Food/Ora beach food.jpg" />
-      </div>
-      <div class="stars">
-        <h2 class="names" id="sos">Sea of Stars</h2>
-        <img class="bigpics" src="../images/Beaches/maldives.jpg" />
-        <img class="sand" src="../images/Beaches/sand.jpg" />
-        <h2 class="activities">Activities</h2>
-        <div>
-          <img class="actpics" src="../images/Activities/Beach Bonfires.jpg" />
-          <img class="actpics" src="../images/Activities/Sea star gazing.jpg" />
-          <img class="actpics" src="../images/Activities/Fishing.jpg" />
-        </div>
-        <h2 class="reviews">Reviews</h2>
-        <img class="foodpics" src="../images/Food/Sea of Stars food.jpg" />
-      </div>
-      <div class="copa">
-        <h2 class="names" id="c">Copacabana</h2>
-        <img class="bigpics" src="../images/Beaches/copacabana.jpg" />
-        <img class="sand" src="../images/Beaches/sand.jpg" />
-        <h2 class="activities">Activities</h2>
-        <div>
-          <img
-            class="actpics"
-            src="../images/Activities/Beach volleyball.jpg"
-          />
-          <img class="actpics" src="../images/Activities/Snorkling.jpg" />
-          <img class="actpics" src="../images/Activities/Surfing.jpg" />
-        </div>
-        <h2 class="reviews">Reviews</h2>
-        <img class="foodpics" src="../images/Food/Copacabana food.jpg" />
-      </div>
-      <div class="rena">
-        <h2 class="names" id="ri">Renaissance Island</h2>
-        <img class="bigpics" src="../images/Beaches/ri.png" />
-        <img class="sand" src="../images/Beaches/sand.jpg" />
-        <h2 class="activities">Activities</h2>
-        <div>
-          <img
-            class="actpics"
-            src="../images/Activities/Flamingo Watching.jpg"
-          />
-          <img class="actpics" src="../images/Activities/Jeep Safari.jpg" />
-          <img class="actpics" src="../images/Activities/Beach Reading.jpg" />
-        </div>
-        <h2 class="reviews">Reviews</h2>
-        <img
-          class="foodpics"
-          src="../images/Food/Renaissance Island food.jpg"
-        />
       </div>
     </div>
   </body>
@@ -99,7 +110,8 @@ export default {
   },
   methods: {
     startAnimations: function () {
-      gsap.to("#maintitle", { duration: 2, color: "black" });
+      gsap.to("#maintitle", { duration: 1, color: "white" });
+      gsap.from("#gallery", { opacity: 0, duration: 3 });
     },
   },
 };
