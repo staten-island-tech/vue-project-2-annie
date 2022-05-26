31a6a27a195c7b972ff8296c99f340b15c7575e6
AgnethaDMello <agnethad@nycstudents.net>
2022-05-26 03:04:20 -0400

    pls no more bugs

--- nuxt-project/nuxt.config.js
+++ nuxt-project/nuxt.config.js
@@ -32,23 +32,24 @@ export default {
     extraPlugins: {
       scrollTrigger: true,
     },
-  buildModules: [
-    'nuxt-gsap-module'
-  ],
-  gsap: {
-    extraPlugins: {
-      scrollTrigger: true
-    }
-  },
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-    // https://go.nuxtjs.dev/content
-    "@nuxt/content",
-  ],
+    buildModules: [
+      'nuxt-gsap-module'
+    ],
+    gsap: {
+      extraPlugins: {
+        scrollTrigger: true
+      }
+    },
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: [
+      // https://go.nuxtjs.dev/content
+      "@nuxt/content",
+    ],
 
-  // Content module configuration: https://go.nuxtjs.dev/config-content
-  content: {},
+    // Content module configuration: https://go.nuxtjs.dev/config-content
+    content: {},
 
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {},
-};
+    // Build Configuration: https://go.nuxtjs.dev/config-build
+    build: {},
+  }
+}
