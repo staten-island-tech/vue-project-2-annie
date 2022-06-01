fa0f79c905d37a1ee3065ca9486900bfcc32da19
AnisaLubonja <90656926+AnisaLubonja@users.noreply.github.com>
2022-05-31 22:30:21 -0400

    Revert "sobs"
    This reverts commit fd68d932c19ea64152400de5561eead0fc15fa3a.

--- nuxt-project/pages/index.vue
+++ nuxt-project/pages/index.vue
@@ -1,3 +1,7 @@
+<style>
+@import "style.css";
+</style>
+
 <template>
   <body id="body">
     <div>
@@ -24,25 +28,25 @@
               <img
                 id="chamimg"
                 class="bigpics"
-                src="../images/Beaches/champagne beach.jpg"
+                src="~../assets/public/images/Beaches/champagnebeach.jpg"
                 alt="Image of Champagne Beach"
               />
               <img
                 id="chamfood"
                 class="foodpics"
-                src="../images/Food/Champagne beach food.jpg"
+                src="~..\assets\public\images\Food\Champagne beach food.jpg"
                 alt="Image of Champagne Beach food"
               />
             </div>
             <img
               class="sand"
-              src="../images/Beaches/sand.jpg"
+              src="~..\assets\public\images\Beaches\sand.jpg"
               alt="Image of sand"
             />
             <h3 class="activities">Activities</h3>
             <div>
               <h4 class="actdesc">
-                Have some fun at Beach Zumba! Dance with your friends all day.
+                Have some fun at Beach Zumba! Dance with your friends!
               </h4>
               <h4 class="actdesc">
                 Play in the sand and make some beautiful sand art!
@@ -57,17 +61,17 @@
               <div>
                 <img
                   class="actpics"
-                  src="../images/Activities/Beach zumba.jpg"
+                  src="~..\assets\public\images\Activities\Beach zumba.jpg"
                   alt="Beach zumba image"
                 />
                 <img
                   class="actpics"
-                  src="../images/Activities/Sand art.jpg"
+                  src="~..\assets\public\images\Activities\Sand art.jpg"
                   alt="Sand art image"
                 />
                 <img
                   class="actpics"
-                  src="../images/Activities/Frisbee.jpg"
+                  src="~..\assets\public\images\Activities\Frisbee.jpg"
                   alt="Frisbee image"
                 />
               </div>
@@ -76,22 +80,22 @@
             <div id="chamrevsdiv">
               <img
                 class="rev1"
-                src="../images/Revs/Cham Rev 1.jpg"
+                src="~..\assets\public\images\Revs\Cham-Rev-1.jpg"
                 alt="Review #1 on Champagne Beach"
               />
               <img
                 class="rev2"
-                src="../images/Revs/Cham Rev 2.jpg"
+                src="~..\assets\public\images\Revs\Cham Rev 2.jpg"
                 alt="Review #2 on Champagne Beach"
               />
               <img
                 class="rev3"
-                src="../images/Revs/Cham Rev 3.jpg"
+                src="~..\assets\public\images\Revs\Cham Rev 3.jpg"
                 alt="Review #3 on Champagne Beach"
               />
               <img
                 class="rev4"
-                src="../images/Revs/Cham Rev 4.jpg"
+                src="~..\assets\public\images\Revs\Cham Rev 4.jpg"
                 alt="Review #4 on Champagne Beach"
               />
             </div>
@@ -108,7 +112,7 @@
               <img
                 class="bigpics"
                 id="oraimg"
-                src="../images/Beaches/ora beach.jpg"
+                src="~../assets/public/images/Beaches/ora beach.jpg"
                 alt="Image of Ora Beach"
               />
               <h3 id="act2" class="activities">Activities</h3>
@@ -130,17 +134,17 @@
                     <cld-transformation height="480" width="480" crop="fill" />
                     <img
                       class="actpics2"
-                      src="../images/Activities/Water Polo.jpg"
+                      src="~..\assets\public\images\Activities\Water polo.jpg"
                       alt="Water polo image"
                     />
                     <img
                       class="actpics2"
-                      src="../images/Activities/Jewelry Making.jpg"
+                      src="~..\assets\public\images\Activities\Jewelry Making.jpg"
                       alt="Jewelry making image"
                     />
                     <img
                       class="actpics2"
-                      src="../images/Activities/Castle construction.jpg"
+                      src="~..\assets\public\images\Activities\Castle construction.jpg"
                       alt="Castle construction image"
                     />
                   </div>
@@ -150,29 +154,29 @@
             <img
               class="foodpics"
               id="orafood"
-              src="../images/Food/Ora beach food.jpg"
+              src="~..\assets\public\images\Food\Ora beach food.jpg"
               alt="Image of Ora Beach food"
             />
             <h3 class="reviews">Reviews</h3>
             <div id="orarevsdiv">
               <img
                 class="rev1"
-                src="../images/Revs/Ora Rev 1.jpg"
+                src="~..\assets\public\images\Revs\Ora Rev 1.jpg"
                 alt="Review #1 on Ora Beach"
               />
               <img
                 class="rev2"
-                src="../images/Revs/Ora Rev 2.jpg"
+                src="~..\assets\public\images\Revs\Ora Rev 2.jpg"
                 alt="Review #2 on Ora Beach"
               />
               <img
                 class="rev3"
-                src="../images/Revs/Ora Rev 3.jpg"
+                src="~..\assets\public\images\Revs\Ora Rev 3.jpg"
                 alt="Review #3 on Ora Beach"
               />
               <img
                 class="rev4"
-                src="../images/Revs/Ora Rev 4.jpg"
+                src="~..\assets\public\images\Revs\Ora Rev 4.jpg"
                 alt="Review #4 on Ora Beach"
               />
             </div>
@@ -183,20 +187,20 @@
               <img
                 id="sosimg"
                 class="bigpics"
-                src="../images/Beaches/maldives.jpg"
+                src="~../assets/public/images/Beaches/maldives.jpg"
                 alt="Image of Sea of Stars"
               />
               <img
                 id="sosfood"
                 class="foodpics"
-                src="../images/Food/Sea of Stars food.jpg"
+                src="~..\assets\public\images\Food\Sea of Stars food.jpg"
                 alt="Image of Sea of Stars food"
               />
             </div>
             <img
               id="sand3"
               class="sand"
-              src="../images/Beaches/sand.jpg"
+              src="~..\assets\public\images\Beaches\sand.jpg"
               alt="Image of sand"
             />
             <h3 id="act3" class="activities">Activities</h3>
@@ -216,19 +220,19 @@
               <img
                 id="fish"
                 class="actpics3"
-                src="../images/Activities/Fishing.jpg"
+                src="~..\assets\public\images\Activities\Fishing.jpg"
                 alt="Fishing image"
               />
               <img
                 class="actpics3"
                 id="stargaze"
-                src="../images/Activities/Sea star gazing.jpg"
+                src="~..\assets\public\images\Activities\Sea star gazing.jpg"
                 alt="Sea star gazing image"
               />
               <img
                 class="actpics3"
                 id="bonfire"
-                src="../images/Activities/Beach Bonfires.jpg"
+                src="~..\assets\public\images\Activities\Beach Bonfires.jpg"
                 alt="Beach bonfires image"
               />
             </div>
@@ -236,22 +240,22 @@
             <div id="sosrevsdiv">
               <img
                 class="rev1"
-                src="../images/Revs/SOS Rev 1.jpg"
+                src="~..\assets\public\images\Revs\SOS Rev 1.JPG"
                 alt="Review #1 on Sea of Stars"
               />
               <img
                 class="rev2"
-                src="../images/Revs/SOS Rev 2.jpg"
+                src="~..\assets\public\images\Revs\SOS Rev 2.JPG"
                 alt="Review #2 on Sea of Stars"
               />
               <img
                 class="rev3"
-                src="../images/Revs/SOS Rev 3.jpg"
+                src="~..\assets\public\images\Revs\SOS Rev 3.JPG"
                 alt="Review #3 on Sea of Stars"
               />
               <img
                 class="rev4"
-                src="../images/Revs/SOS Rev 4.jpg"
+                src="~..\assets\public\images\Revs\SOS Rev 4.JPG"
                 alt="Review #4 on Sea of Stars"
               />
             </div>
@@ -268,7 +272,7 @@
               <img
                 class="bigpics"
                 id="cbimg"
-                src="../images/Beaches/copacabana.jpg"
+                src="~../assets/public/images/Beaches/copacabana.jpg"
                 alt="Image of Copacabana Beach"
               />
               <h3 id="act4" class="activities">Activities</h3>
@@ -292,17 +296,17 @@
                     <cld-transformation height="480" width="480" crop="fill" />
                     <img
                       class="actpics4"
-                      src="../images/Activities/Beach volleyball.jpg"
+                      src="~..\assets\public\images\Activities\Beach volleyball.jpg"
                       alt="Beach volleyball image"
                     />
                     <img
                       class="actpics4"
-                      src="../images/Activities/Snorkling.jpg"
+                      src="~..\assets\public\images\Activities\Snorkling.jpg"
                       alt="Snorkling image"
                     />
                     <img
                       class="actpics4"
-                      src="../images/Activities/Surfing.jpg"
+                      src="~..\assets\public\images\Activities\Surfing.jpg"
                       alt="Surfing image"
                     />
                   </div>
@@ -312,29 +316,29 @@
             <img
               class="foodpics"
               id="cbfood"
-              src="../images/Food/Copacabana food.jpg"
+              src="~..\assets\public\images\Food\Copacabana food.jpg"
               alt="Image of Copacabana Beach food"
             />
             <h3 class="reviews">Reviews</h3>
             <div id="coprevsdiv">
               <img
                 class="rev1"
-                src="../images/Revs/Cop Rev 1.jpg"
+                src="~..\assets\public\images\Revs\Cop Rev 1.JPG"
                 alt="Review #1 on Copacabana Beach"
               />
               <img
                 class="rev2"
-                src="../images/Revs/Cop Rev 2.jpg"
+                src="~..\assets\public\images\Revs\Cop Rev 2.JPG"
                 alt="Review #2 on Copacabana Beach"
               />
               <img
                 class="rev3"
-                src="../images/Revs/Cop Rev 3.jpg"
+                src="~..\assets\public\images\Revs\Cop Rev 3.JPG"
                 alt="Review #3 on Copacabana Beach"
               />
               <img
                 class="rev4"
-                src="../images/Revs/Cop Rev 4.jpg"
+                src="~..\assets\public\images\Revs\Cop Rev 4.JPG"
                 alt="Review #4 on Copacabana Beach"
               />
             </div>
@@ -343,7 +347,7 @@
             <h2 id="riname" class="names">Renaissance Island</h2>
             <img
               class="sand"
-              src="../images/Beaches/sand.jpg"
+              src="~..\assets\public\images\Beaches\sand.jpg"
               alt="Image of sand"
             />
             <h3 class="activities">Activities</h3>
@@ -361,17 +365,17 @@
               <div>
                 <img
                   class="actpics5"
-                  src="../images/Activities/Flamingo Watching.jpg"
+                  src="~..\assets\public\images\Activities\Flamingo Watching.jpg"
                   alt="Flamingo watching image"
                 />
                 <img
                   class="actpics5"
-                  src="../images/Activities/Jeep Safari.jpg"
+                  src="~..\assets\public\images\Activities\Jeep Safari.jpg"
                   alt="Jeep safari image"
                 />
                 <img
                   class="actpics5"
-                  src="../images/Activities/Beach Reading.jpg"
+                  src="~..\assets\public\images\Activities\Beach Reading.jpg"
                   alt="Beach reading image"
                 />
               </div>
@@ -380,35 +384,35 @@
           <img
             id="rifood"
             class="foodpics"
-            src="../images/Food/Renaissance Island food.jpg"
+            src="~..\assets\public\images\Food\Renaissance Island food.jpg"
             alt="Image of Renaissance Island food"
           />
           <img
             id="riimg"
             class="bigpics"
-            src="../images/Beaches/ri.png"
+            src="~../assets/public/images/Beaches/ri.png"
             alt="Image of Renaissance Island"
           />
           <h3 class="reviews">Reviews</h3>
           <div id="rirevsdiv">
             <img
               class="rev1"
-              src="../images/Revs/RI Rev 1.jpg"
+              src="~../assets/public/images/Revs/RI-Rev-1.jpg"
               alt="Review #1 on Renaissance Island"
             />
             <img
               class="rev2"
-              src="../images/Revs/RI Rev 2.jpg"
+              src="~../assets/public/images/Revs/RI Rev 2.jpg"
               alt="Review #2 on Renaissance Island"
             />
             <img
               class="rev3"
-              src="../images/Revs/RI Rev 3.jpg"
+              src="~../assets/public/images/Revs/RI Rev 3.jpg"
               alt="Review #3 on Renaissance Island"
             />
             <img
               class="rev4"
-              src="../images/Revs/RI Rev 4.jpg"
+              src="~../assets/public/images/Revs/RI Rev 4.jpg"
               alt="Review #4 on Renaissance Island"
             />
           </div>
@@ -419,8 +423,12 @@
 </template>
 
 <script>
+import style from "@/style/"
 export default {
   name: "IndexPage",
+  components: {
+    style,
+  },
   mounted: function () {
     this.startAnimations();
     const initUserTheme = this.getTheme() || this.getMediaPreference();
@@ -501,6 +509,4 @@ export default {
 };
 </script>
 
-<style>
-@import "style.css";
-</style>
+
